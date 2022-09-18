e3b6ee61365f162d5edb1ca734131f729f291100
wangyq <985759262@qq.com>
2022-09-18 12:16:52 +0800

    refactor(proto): logic

--- internal/logic/proto/logic.pb.go
+++ internal/logic/proto/logic.pb.go
@@ -21,17 +21,75 @@ const (
 	_ = protoimpl.EnforceVersion(protoimpl.MaxVersion - 20)
 )
 
-type Error struct {
+type C2CMsg_Kind int32
+
+const (
+	C2CMsg_NONE          C2CMsg_Kind = 0
+	C2CMsg_REQ_C_SRV     C2CMsg_Kind = 1
+	C2CMsg_REQ_SYN_SRV_C C2CMsg_Kind = 2
+	C2CMsg_REQ_SRV_C     C2CMsg_Kind = 3
+)
+
+// Enum value maps for C2CMsg_Kind.
+var (
+	C2CMsg_Kind_name = map[int32]string{
+		0: "NONE",
+		1: "REQ_C_SRV",
+		2: "REQ_SYN_SRV_C",
+		3: "REQ_SRV_C",
+	}
+	C2CMsg_Kind_value = map[string]int32{
+		"NONE":          0,
+		"REQ_C_SRV":     1,
+		"REQ_SYN_SRV_C": 2,
+		"REQ_SRV_C":     3,
+	}
+)
+
+func (x C2CMsg_Kind) Enum() *C2CMsg_Kind {
+	p := new(C2CMsg_Kind)
+	*p = x
+	return p
+}
+
+func (x C2CMsg_Kind) String() string {
+	return protoimpl.X.EnumStringOf(x.Descriptor(), protoreflect.EnumNumber(x))
+}
+
+func (C2CMsg_Kind) Descriptor() protoreflect.EnumDescriptor {
+	return file_internal_logic_proto_logic_proto_enumTypes[0].Descriptor()
+}
+
+func (C2CMsg_Kind) Type() protoreflect.EnumType {
+	return &file_internal_logic_proto_logic_proto_enumTypes[0]
+}
+
+func (x C2CMsg_Kind) Number() protoreflect.EnumNumber {
+	return protoreflect.EnumNumber(x)
+}
+
+// Deprecated: Use C2CMsg_Kind.Descriptor instead.
+func (C2CMsg_Kind) EnumDescriptor() ([]byte, []int) {
+	return file_internal_logic_proto_logic_proto_rawDescGZIP(), []int{0, 0}
+}
+
+type C2CMsg struct {
 	state         protoimpl.MessageState
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
 
-	Code int32  `protobuf:"varint,1,opt,name=code,proto3" json:"code,omitempty"`
-	Msg  string `protobuf:"bytes,2,opt,name=msg,proto3" json:"msg,omitempty"`
+	Kind C2CMsg_Kind `protobuf:"varint,1,opt,name=kind,proto3,enum=logic.C2CMsg_Kind" json:"kind,omitempty"`
+	// Types that are assignable to UserId:
+	//
+	//	*C2CMsg_From
+	//	*C2CMsg_To
+	UserId isC2CMsg_UserId `protobuf_oneof:"user_id"`
+	MsgId  uint64          `protobuf:"varint,4,opt,name=msg_id,json=msgId,proto3" json:"msg_id,omitempty"`
+	Data   *anypb.Any      `protobuf:"bytes,5,opt,name=data,proto3" json:"data,omitempty"`
 }
 
-func (x *Error) Reset() {
-	*x = Error{}
+func (x *C2CMsg) Reset() {
+	*x = C2CMsg{}
 	if protoimpl.UnsafeEnabled {
 		mi := &file_internal_logic_proto_logic_proto_msgTypes[0]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
@@ -39,13 +97,13 @@ func (x *Error) Reset() {
 	}
 }
 
-func (x *Error) String() string {
+func (x *C2CMsg) String() string {
 	return protoimpl.X.MessageStringOf(x)
 }
 
-func (*Error) ProtoMessage() {}
+func (*C2CMsg) ProtoMessage() {}
 
-func (x *Error) ProtoReflect() protoreflect.Message {
+func (x *C2CMsg) ProtoReflect() protoreflect.Message {
 	mi := &file_internal_logic_proto_logic_proto_msgTypes[0]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
@@ -57,101 +115,86 @@ func (x *Error) ProtoReflect() protoreflect.Message {
 	return mi.MessageOf(x)
 }
 
-// Deprecated: Use Error.ProtoReflect.Descriptor instead.
-func (*Error) Descriptor() ([]byte, []int) {
+// Deprecated: Use C2CMsg.ProtoReflect.Descriptor instead.
+func (*C2CMsg) Descriptor() ([]byte, []int) {
 	return file_internal_logic_proto_logic_proto_rawDescGZIP(), []int{0}
 }
 
-func (x *Error) GetCode() int32 {
+func (x *C2CMsg) GetKind() C2CMsg_Kind {
 	if x != nil {
-		return x.Code
+		return x.Kind
+	}
+	return C2CMsg_NONE
+}
+
+func (m *C2CMsg) GetUserId() isC2CMsg_UserId {
+	if m != nil {
+		return m.UserId
+	}
+	return nil
+}
+
+func (x *C2CMsg) GetFrom() uint64 {
+	if x, ok := x.GetUserId().(*C2CMsg_From); ok {
+		return x.From
 	}
 	return 0
 }
 
-func (x *Error) GetMsg() string {
-	if x != nil {
-		return x.Msg
+func (x *C2CMsg) GetTo() uint64 {
+	if x, ok := x.GetUserId().(*C2CMsg_To); ok {
+		return x.To
 	}
-	return ""
+	return 0
 }
 
-type C2CMsgReq struct {
-	state         protoimpl.MessageState
-	sizeCache     protoimpl.SizeCache
-	unknownFields protoimpl.UnknownFields
-
-	MsgId uint64     `protobuf:"varint,1,opt,name=msg_id,json=msgId,proto3" json:"msg_id,omitempty"`
-	To    uint64     `protobuf:"varint,2,opt,name=to,proto3" json:"to,omitempty"`
-	Data  *anypb.Any `protobuf:"bytes,3,opt,name=data,proto3" json:"data,omitempty"`
-}
-
-func (x *C2CMsgReq) Reset() {
-	*x = C2CMsgReq{}
-	if protoimpl.UnsafeEnabled {
-		mi := &file_internal_logic_proto_logic_proto_msgTypes[1]
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		ms.StoreMessageInfo(mi)
-	}
-}
-
-func (x *C2CMsgReq) String() string {
-	return protoimpl.X.MessageStringOf(x)
-}
-
-func (*C2CMsgReq) ProtoMessage() {}
-
-func (x *C2CMsgReq) ProtoReflect() protoreflect.Message {
-	mi := &file_internal_logic_proto_logic_proto_msgTypes[1]
-	if protoimpl.UnsafeEnabled && x != nil {
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		if ms.LoadMessageInfo() == nil {
-			ms.StoreMessageInfo(mi)
-		}
-		return ms
-	}
-	return mi.MessageOf(x)
-}
-
-// Deprecated: Use C2CMsgReq.ProtoReflect.Descriptor instead.
-func (*C2CMsgReq) Descriptor() ([]byte, []int) {
-	return file_internal_logic_proto_logic_proto_rawDescGZIP(), []int{1}
-}
-
-func (x *C2CMsgReq) GetMsgId() uint64 {
+func (x *C2CMsg) GetMsgId() uint64 {
 	if x != nil {
 		return x.MsgId
 	}
 	return 0
 }
 
-func (x *C2CMsgReq) GetTo() uint64 {
-	if x != nil {
-		return x.To
-	}
-	return 0
-}
-
-func (x *C2CMsgReq) GetData() *anypb.Any {
+func (x *C2CMsg) GetData() *anypb.Any {
 	if x != nil {
 		return x.Data
 	}
 	return nil
 }
 
+type isC2CMsg_UserId interface {
+	isC2CMsg_UserId()
+}
+
+type C2CMsg_From struct {
+	From uint64 `protobuf:"varint,2,opt,name=from,proto3,oneof"`
+}
+
+type C2CMsg_To struct {
+	To uint64 `protobuf:"varint,3,opt,name=to,proto3,oneof"`
+}
+
+func (*C2CMsg_From) isC2CMsg_UserId() {}
+
+func (*C2CMsg_To) isC2CMsg_UserId() {}
+
 type Ack struct {
 	state         protoimpl.MessageState
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
 
-	Error  *Error   `protobuf:"bytes,1,opt,name=error,proto3" json:"error,omitempty"`
-	MsgIds []uint64 `protobuf:"varint,2,rep,packed,name=msg_ids,json=msgIds,proto3" json:"msg_ids,omitempty"`
+	// Types that are assignable to UserId:
+	//
+	//	*Ack_From
+	//	*Ack_To
+	UserId isAck_UserId `protobuf_oneof:"user_id"`
+	MsgId  uint64       `protobuf:"varint,3,opt,name=msg_id,json=msgId,proto3" json:"msg_id,omitempty"`
 }
 
 func (x *Ack) Reset() {
 	*x = Ack{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_internal_logic_proto_logic_proto_msgTypes[2]
+		mi := &file_internal_logic_proto_logic_proto_msgTypes[1]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -164,7 +207,7 @@ func (x *Ack) String() string {
 func (*Ack) ProtoMessage() {}
 
 func (x *Ack) ProtoReflect() protoreflect.Message {
-	mi := &file_internal_logic_proto_logic_proto_msgTypes[2]
+	mi := &file_internal_logic_proto_logic_proto_msgTypes[1]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -177,69 +220,52 @@ func (x *Ack) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use Ack.ProtoReflect.Descriptor instead.
 func (*Ack) Descriptor() ([]byte, []int) {
-	return file_internal_logic_proto_logic_proto_rawDescGZIP(), []int{2}
+	return file_internal_logic_proto_logic_proto_rawDescGZIP(), []int{1}
 }
 
-func (x *Ack) GetError() *Error {
-	if x != nil {
-		return x.Error
+func (m *Ack) GetUserId() isAck_UserId {
+	if m != nil {
+		return m.UserId
 	}
 	return nil
 }
 
-func (x *Ack) GetMsgIds() []uint64 {
+func (x *Ack) GetFrom() uint64 {
+	if x, ok := x.GetUserId().(*Ack_From); ok {
+		return x.From
+	}
+	return 0
+}
+
+func (x *Ack) GetTo() uint64 {
+	if x, ok := x.GetUserId().(*Ack_To); ok {
+		return x.To
+	}
+	return 0
+}
+
+func (x *Ack) GetMsgId() uint64 {
 	if x != nil {
-		return x.MsgIds
+		return x.MsgId
 	}
-	return nil
+	return 0
 }
 
-type C2CMsgRsp struct {
-	state         protoimpl.MessageState
-	sizeCache     protoimpl.SizeCache
-	unknownFields protoimpl.UnknownFields
-
-	MsgIds []uint64 `protobuf:"varint,1,rep,packed,name=msg_ids,json=msgIds,proto3" json:"msg_ids,omitempty"`
+type isAck_UserId interface {
+	isAck_UserId()
 }
 
-func (x *C2CMsgRsp) Reset() {
-	*x = C2CMsgRsp{}
-	if protoimpl.UnsafeEnabled {
-		mi := &file_internal_logic_proto_logic_proto_msgTypes[3]
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		ms.StoreMessageInfo(mi)
-	}
+type Ack_From struct {
+	From uint64 `protobuf:"varint,1,opt,name=from,proto3,oneof"`
 }
 
-func (x *C2CMsgRsp) String() string {
-	return protoimpl.X.MessageStringOf(x)
+type Ack_To struct {
+	To uint64 `protobuf:"varint,2,opt,name=to,proto3,oneof"`
 }
 
-func (*C2CMsgRsp) ProtoMessage() {}
+func (*Ack_From) isAck_UserId() {}
 
-func (x *C2CMsgRsp) ProtoReflect() protoreflect.Message {
-	mi := &file_internal_logic_proto_logic_proto_msgTypes[3]
-	if protoimpl.UnsafeEnabled && x != nil {
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		if ms.LoadMessageInfo() == nil {
-			ms.StoreMessageInfo(mi)
-		}
-		return ms
-	}
-	return mi.MessageOf(x)
-}
-
-// Deprecated: Use C2CMsgRsp.ProtoReflect.Descriptor instead.
-func (*C2CMsgRsp) Descriptor() ([]byte, []int) {
-	return file_internal_logic_proto_logic_proto_rawDescGZIP(), []int{3}
-}
-
-func (x *C2CMsgRsp) GetMsgIds() []uint64 {
-	if x != nil {
-		return x.MsgIds
-	}
-	return nil
-}
+func (*Ack_To) isAck_UserId() {}
 
 var File_internal_logic_proto_logic_proto protoreflect.FileDescriptor
 
@@ -248,25 +274,29 @@ var file_internal_logic_proto_logic_proto_rawDesc = []byte{
 	0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2f, 0x6c, 0x6f, 0x67, 0x69, 0x63, 0x2e, 0x70, 0x72, 0x6f,
 	0x74, 0x6f, 0x12, 0x05, 0x6c, 0x6f, 0x67, 0x69, 0x63, 0x1a, 0x19, 0x67, 0x6f, 0x6f, 0x67, 0x6c,
 	0x65, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x2f, 0x61, 0x6e, 0x79, 0x2e, 0x70,
-	0x72, 0x6f, 0x74, 0x6f, 0x22, 0x2d, 0x0a, 0x05, 0x45, 0x72, 0x72, 0x6f, 0x72, 0x12, 0x12, 0x0a,
-	0x04, 0x63, 0x6f, 0x64, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x05, 0x52, 0x04, 0x63, 0x6f, 0x64,
-	0x65, 0x12, 0x10, 0x0a, 0x03, 0x6d, 0x73, 0x67, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x03,
-	0x6d, 0x73, 0x67, 0x22, 0x5c, 0x0a, 0x09, 0x43, 0x32, 0x43, 0x4d, 0x73, 0x67, 0x52, 0x65, 0x71,
-	0x12, 0x15, 0x0a, 0x06, 0x6d, 0x73, 0x67, 0x5f, 0x69, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x04,
-	0x52, 0x05, 0x6d, 0x73, 0x67, 0x49, 0x64, 0x12, 0x0e, 0x0a, 0x02, 0x74, 0x6f, 0x18, 0x02, 0x20,
-	0x01, 0x28, 0x04, 0x52, 0x02, 0x74, 0x6f, 0x12, 0x28, 0x0a, 0x04, 0x64, 0x61, 0x74, 0x61, 0x18,
-	0x03, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x14, 0x2e, 0x67, 0x6f, 0x6f, 0x67, 0x6c, 0x65, 0x2e, 0x70,
-	0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x2e, 0x41, 0x6e, 0x79, 0x52, 0x04, 0x64, 0x61, 0x74,
-	0x61, 0x22, 0x42, 0x0a, 0x03, 0x41, 0x63, 0x6b, 0x12, 0x22, 0x0a, 0x05, 0x65, 0x72, 0x72, 0x6f,
-	0x72, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x0c, 0x2e, 0x6c, 0x6f, 0x67, 0x69, 0x63, 0x2e,
-	0x45, 0x72, 0x72, 0x6f, 0x72, 0x52, 0x05, 0x65, 0x72, 0x72, 0x6f, 0x72, 0x12, 0x17, 0x0a, 0x07,
-	0x6d, 0x73, 0x67, 0x5f, 0x69, 0x64, 0x73, 0x18, 0x02, 0x20, 0x03, 0x28, 0x04, 0x52, 0x06, 0x6d,
-	0x73, 0x67, 0x49, 0x64, 0x73, 0x22, 0x24, 0x0a, 0x09, 0x43, 0x32, 0x43, 0x4d, 0x73, 0x67, 0x52,
-	0x73, 0x70, 0x12, 0x17, 0x0a, 0x07, 0x6d, 0x73, 0x67, 0x5f, 0x69, 0x64, 0x73, 0x18, 0x01, 0x20,
-	0x03, 0x28, 0x04, 0x52, 0x06, 0x6d, 0x73, 0x67, 0x49, 0x64, 0x73, 0x42, 0x23, 0x5a, 0x21, 0x67,
-	0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x6a, 0x64, 0x78, 0x6a, 0x2f, 0x63,
-	0x79, 0x62, 0x65, 0x72, 0x2d, 0x77, 0x61, 0x67, 0x6f, 0x6e, 0x2f, 0x6c, 0x6f, 0x67, 0x69, 0x63,
-	0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x72, 0x6f, 0x74, 0x6f, 0x22, 0xe7, 0x01, 0x0a, 0x06, 0x43, 0x32, 0x43, 0x4d, 0x73, 0x67, 0x12,
+	0x26, 0x0a, 0x04, 0x6b, 0x69, 0x6e, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0e, 0x32, 0x12, 0x2e,
+	0x6c, 0x6f, 0x67, 0x69, 0x63, 0x2e, 0x43, 0x32, 0x43, 0x4d, 0x73, 0x67, 0x2e, 0x4b, 0x69, 0x6e,
+	0x64, 0x52, 0x04, 0x6b, 0x69, 0x6e, 0x64, 0x12, 0x14, 0x0a, 0x04, 0x66, 0x72, 0x6f, 0x6d, 0x18,
+	0x02, 0x20, 0x01, 0x28, 0x04, 0x48, 0x00, 0x52, 0x04, 0x66, 0x72, 0x6f, 0x6d, 0x12, 0x10, 0x0a,
+	0x02, 0x74, 0x6f, 0x18, 0x03, 0x20, 0x01, 0x28, 0x04, 0x48, 0x00, 0x52, 0x02, 0x74, 0x6f, 0x12,
+	0x15, 0x0a, 0x06, 0x6d, 0x73, 0x67, 0x5f, 0x69, 0x64, 0x18, 0x04, 0x20, 0x01, 0x28, 0x04, 0x52,
+	0x05, 0x6d, 0x73, 0x67, 0x49, 0x64, 0x12, 0x28, 0x0a, 0x04, 0x64, 0x61, 0x74, 0x61, 0x18, 0x05,
+	0x20, 0x01, 0x28, 0x0b, 0x32, 0x14, 0x2e, 0x67, 0x6f, 0x6f, 0x67, 0x6c, 0x65, 0x2e, 0x70, 0x72,
+	0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x2e, 0x41, 0x6e, 0x79, 0x52, 0x04, 0x64, 0x61, 0x74, 0x61,
+	0x22, 0x41, 0x0a, 0x04, 0x4b, 0x69, 0x6e, 0x64, 0x12, 0x08, 0x0a, 0x04, 0x4e, 0x4f, 0x4e, 0x45,
+	0x10, 0x00, 0x12, 0x0d, 0x0a, 0x09, 0x52, 0x45, 0x51, 0x5f, 0x43, 0x5f, 0x53, 0x52, 0x56, 0x10,
+	0x01, 0x12, 0x11, 0x0a, 0x0d, 0x52, 0x45, 0x51, 0x5f, 0x53, 0x59, 0x4e, 0x5f, 0x53, 0x52, 0x56,
+	0x5f, 0x43, 0x10, 0x02, 0x12, 0x0d, 0x0a, 0x09, 0x52, 0x45, 0x51, 0x5f, 0x53, 0x52, 0x56, 0x5f,
+	0x43, 0x10, 0x03, 0x42, 0x09, 0x0a, 0x07, 0x75, 0x73, 0x65, 0x72, 0x5f, 0x69, 0x64, 0x22, 0x4f,
+	0x0a, 0x03, 0x41, 0x63, 0x6b, 0x12, 0x14, 0x0a, 0x04, 0x66, 0x72, 0x6f, 0x6d, 0x18, 0x01, 0x20,
+	0x01, 0x28, 0x04, 0x48, 0x00, 0x52, 0x04, 0x66, 0x72, 0x6f, 0x6d, 0x12, 0x10, 0x0a, 0x02, 0x74,
+	0x6f, 0x18, 0x02, 0x20, 0x01, 0x28, 0x04, 0x48, 0x00, 0x52, 0x02, 0x74, 0x6f, 0x12, 0x15, 0x0a,
+	0x06, 0x6d, 0x73, 0x67, 0x5f, 0x69, 0x64, 0x18, 0x03, 0x20, 0x01, 0x28, 0x04, 0x52, 0x05, 0x6d,
+	0x73, 0x67, 0x49, 0x64, 0x42, 0x09, 0x0a, 0x07, 0x75, 0x73, 0x65, 0x72, 0x5f, 0x69, 0x64, 0x42,
+	0x23, 0x5a, 0x21, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x6a, 0x64,
+	0x78, 0x6a, 0x2f, 0x63, 0x79, 0x62, 0x65, 0x72, 0x2d, 0x77, 0x61, 0x67, 0x6f, 0x6e, 0x2f, 0x6c,
+	0x6f, 0x67, 0x69, 0x63, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -281,17 +311,17 @@ func file_internal_logic_proto_logic_proto_rawDescGZIP() []byte {
 	return file_internal_logic_proto_logic_proto_rawDescData
 }
 
-var file_internal_logic_proto_logic_proto_msgTypes = make([]protoimpl.MessageInfo, 4)
+var file_internal_logic_proto_logic_proto_enumTypes = make([]protoimpl.EnumInfo, 1)
+var file_internal_logic_proto_logic_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
 var file_internal_logic_proto_logic_proto_goTypes = []interface{}{
-	(*Error)(nil),     // 0: logic.Error
-	(*C2CMsgReq)(nil), // 1: logic.C2CMsgReq
+	(C2CMsg_Kind)(0),  // 0: logic.C2CMsg.Kind
+	(*C2CMsg)(nil),    // 1: logic.C2CMsg
 	(*Ack)(nil),       // 2: logic.Ack
-	(*C2CMsgRsp)(nil), // 3: logic.C2CMsgRsp
-	(*anypb.Any)(nil), // 4: google.protobuf.Any
+	(*anypb.Any)(nil), // 3: google.protobuf.Any
 }
 var file_internal_logic_proto_logic_proto_depIdxs = []int32{
-	4, // 0: logic.C2CMsgReq.data:type_name -> google.protobuf.Any
-	0, // 1: logic.Ack.error:type_name -> logic.Error
+	0, // 0: logic.C2CMsg.kind:type_name -> logic.C2CMsg.Kind
+	3, // 1: logic.C2CMsg.data:type_name -> google.protobuf.Any
 	2, // [2:2] is the sub-list for method output_type
 	2, // [2:2] is the sub-list for method input_type
 	2, // [2:2] is the sub-list for extension type_name
@@ -306,7 +336,7 @@ func file_internal_logic_proto_logic_proto_init() {
 	}
 	if !protoimpl.UnsafeEnabled {
 		file_internal_logic_proto_logic_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*Error); i {
+			switch v := v.(*C2CMsg); i {
 			case 0:
 				return &v.state
 			case 1:
@@ -318,18 +348,6 @@ func file_internal_logic_proto_logic_proto_init() {
 			}
 		}
 		file_internal_logic_proto_logic_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*C2CMsgReq); i {
-			case 0:
-				return &v.state
-			case 1:
-				return &v.sizeCache
-			case 2:
-				return &v.unknownFields
-			default:
-				return nil
-			}
-		}
-		file_internal_logic_proto_logic_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*Ack); i {
 			case 0:
 				return &v.state
@@ -341,31 +359,28 @@ func file_internal_logic_proto_logic_proto_init() {
 				return nil
 			}
 		}
-		file_internal_logic_proto_logic_proto_msgTypes[3].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*C2CMsgRsp); i {
-			case 0:
-				return &v.state
-			case 1:
-				return &v.sizeCache
-			case 2:
-				return &v.unknownFields
-			default:
-				return nil
-			}
-		}
+	}
+	file_internal_logic_proto_logic_proto_msgTypes[0].OneofWrappers = []interface{}{
+		(*C2CMsg_From)(nil),
+		(*C2CMsg_To)(nil),
+	}
+	file_internal_logic_proto_logic_proto_msgTypes[1].OneofWrappers = []interface{}{
+		(*Ack_From)(nil),
+		(*Ack_To)(nil),
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_internal_logic_proto_logic_proto_rawDesc,
-			NumEnums:      0,
-			NumMessages:   4,
+			NumEnums:      1,
+			NumMessages:   2,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
 		GoTypes:           file_internal_logic_proto_logic_proto_goTypes,
 		DependencyIndexes: file_internal_logic_proto_logic_proto_depIdxs,
+		EnumInfos:         file_internal_logic_proto_logic_proto_enumTypes,
 		MessageInfos:      file_internal_logic_proto_logic_proto_msgTypes,
 	}.Build()
 	File_internal_logic_proto_logic_proto = out.File
